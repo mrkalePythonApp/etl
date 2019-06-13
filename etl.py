4d3fb9a80c7659fbd2027d6d39d45990ad007640
Libor Gabaj <gabaj.libor@slsp.sk>
2019-06-13 17:07:11 +0200

    Individual codelist migration.

---
+++ etl.py
@@ -0,0 +1,380 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Script for migrating individual code list table."""
+__version__ = "0.1.0"
+__status__ = "Alpha"
+__author__ = "Libor Gabaj"
+__copyright__ = "Copyright 2019, " + __author__
+__credits__ = [__author__]
+__license__ = "MIT"
+__maintainer__ = __author__
+__email__ = "records.gabaj@gmail.com"
+
+# Standard library modules
+import os
+import os.path
+import argparse
+import logging
+import mysql.connector as mysql
+
+# Third party modules
+import dbconfig as db
+import sql
+
+
+###############################################################################
+# Script global variables
+###############################################################################
+cmdline = None  # Object with command line arguments
+logger = None  # Object with standard logging
+
+
+###############################################################################
+# Enumeration and parameter classes
+###############################################################################
+class Script:
+    """Script parameters."""
+
+    (
+        fullname, basename, name,
+    ) = ('', '', '',)
+
+
+class Source:
+    """Status parameters of the data source."""
+
+    (
+        conn, query, cursor, table, database
+    ) = (None, None, None, None, None,)
+
+
+class Target:
+    """Status parameters of the data target."""
+
+    (
+        conn, query, cursor, table, database,
+    ) = (None, None, None, None, None,)
+
+
+###############################################################################
+# Actions
+###############################################################################
+def connect_db(config):
+    """Connect to a database.
+
+    Arguments
+    ---------
+    config : dict
+        Connection configuration to a database.
+
+    Returns
+    -------
+    connection : object
+        Connection object to a database.
+
+    Raises
+    -------
+    mysql.connector.Error
+        Native exception of the database connector.
+
+    """
+    try:
+        conn = mysql.connect(**config)
+        logger.info("Database %s connected", config['database'])
+        return conn
+    except mysql.Error as err:
+        if err.errno == mysql.errorcode.ER_ACCESS_DENIED_ERROR:
+            logger.error("Bad database %s credentials", config['database'])
+        elif err.errno == mysql.errorcode.ER_BAD_DB_ERROR:
+            logger.error("Database %s does not exist", config['database'])
+        else:
+            logger.error(err)
+        raise
+
+
+def source_open():
+    """Connect to a source database.
+
+    Returns
+    -------
+    boolean
+        Flag about successful processing.
+
+    """
+    if Source.conn is None:
+        try:
+            Source.conn = connect_db(db.source_config)
+        except Exception:
+            logger.error(
+                'Cannot connect to the source database %s',
+                db.source_config['database']
+                )
+            return False
+    return True
+
+
+def source_close():
+    """Close cursor and connection to a source database."""
+    # Close cursor
+    if Source.cursor is not None:
+        Source.cursor.close()
+    # Close connection to database
+    if Source.conn is not None:
+        Source.conn.close()
+    Source.conn = None
+    Source.query = None
+    Source.cursor = None
+    Source.table = None
+
+
+def target_open():
+    """Connect to a target database.
+
+    Returns
+    -------
+    boolean
+        Flag about successful processing.
+
+    """
+    if Target.conn is None:
+        try:
+            Target.conn = connect_db(db.target_config)
+        except Exception:
+            logger.error(
+                'Cannot connect to the target database %s',
+                db.source_config['database']
+                )
+            return False
+    return True
+
+
+def target_close():
+    """Close cursor and connection to a target database."""
+    # Close cursor
+    if Target.cursor is not None:
+        Target.cursor.close()
+    # Close connection to database
+    if Target.conn is not None:
+        Target.conn.close()
+    Target.conn = None
+    Target.query = None
+    Target.cursor = None
+    Target.table = None
+
+
+def target_truncate(table):
+    """Empty table in the target database.
+
+    Arguments
+    ---------
+    table : str
+        Root name of a database table to be truncated.
+
+    Returns
+    -------
+    boolean
+        Flag about successful processing.
+
+    """
+    # Connect to the database
+    if not target_open():
+        return False
+    # Execute query
+    Target.table = sql.table_compose(
+        table_prefix=sql.target_codelist_prefix,
+        table_root=table
+        )
+    Target.query = sql.query_compose(
+        query_string=sql.table_truncate,
+        table_name=Target.table
+        )
+    Target.cursor = Target.conn.cursor()
+    try:
+        Target.cursor.execute(Target.query)
+        Target.cursor.close()
+        logger.info(
+            "Table %s.%s truncated",
+            db.target_config['database'],
+            Target.table
+            )
+    except mysql.Error as err:
+        logger.error(err)
+        target_close()
+        return False
+    return True
+
+
+def migrate_codelist(codelist):
+    """Migrate content of a source code list to target one.
+
+    Arguments
+    ---------
+    codelist : str
+        Root name of a code list database table.
+
+    Returns
+    -------
+    boolean
+        Flag about successful processing.
+
+    """
+    # Read source table
+    Source.table = sql.source_codelist(codelist)
+    Source.query = sql.compose_select(
+        table=Source.table,
+        fields=sql.source_fields['codelist'],
+        )
+    Source.cursor = Source.conn.cursor(dictionary=True)
+    try:
+        Source.cursor.execute(Source.query)
+        records = Source.cursor.fetchall()
+        logger.debug(
+            'Read %d records from table %s.%s',
+            Source.cursor.rowcount,
+            Source.database,
+            Source.table
+            )
+    except mysql.Error as err:
+        logger.error(err)
+        return False
+    # Truncate target table
+    Target.table = sql.target_codelist(sql.map_codelist[codelist])
+    Target.query = sql.compose_truncate(Target.table)
+    Target.cursor = Target.conn.cursor()
+    try:
+        Target.cursor.execute(Target.query)
+        logger.debug(
+            'Table %s.%s truncated',
+            Target.database,
+            Target.table
+            )
+    except mysql.Error as err:
+        logger.error(err)
+        return False
+    # Insert to target table
+    Target.query = sql.compose_insert(
+        table=Target.table,
+        fields=sql.target_fields['codelist']['fields'],
+        values=sql.target_fields['codelist']['values'],
+        )
+    Target.cursor = Target.conn.cursor()
+    try:
+        Target.cursor.executemany(Target.query, records)
+        logger.debug(
+            'Inserted %d records to table %s.%s',
+            Target.cursor.rowcount,
+            Target.database,
+            Target.table
+            )
+    except mysql.Error as err:
+        logger.error(err)
+        return False
+    # Update user in target table
+    Target.query = sql.compose_update(
+        table=Target.table,
+        fields=sql.target_users,
+        )
+    Target.cursor = Target.conn.cursor()
+    try:
+        Target.cursor.execute(Target.query, {'user': cmdline.user})
+        logger.debug(
+            'Updated %d records in table %s.%s',
+            Target.cursor.rowcount,
+            Target.database,
+            Target.table
+            )
+    except mysql.Error as err:
+        logger.error(err)
+        return False
+    # Success
+    logger.info(
+        'Codelist %s.%s migrated to %s.%s with %d records',
+        Source.database,
+        Source.table,
+        Target.database,
+        Target.table,
+        Source.cursor.rowcount
+        )
+    return True
+
+
+###############################################################################
+# Setup functions
+###############################################################################
+def setup_params():
+    """Determine script operational parameters."""
+    global service_flag
+    Script.fullname = os.path.splitext(os.path.abspath(__file__))[0]
+    Script.basename = os.path.basename(__file__)
+    Script.name = os.path.splitext(Script.basename)[0]
+
+
+def setup_cmdline():
+    """Define command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="ETL for individual code list, version "
+        + __version__
+    )
+    # Options
+    parser.add_argument(
+        "-V", "--version",
+        action="version",
+        version=__version__,
+        help="Current version of the script."
+    )
+    parser.add_argument(
+        "-v", "--verbose",
+        choices=["debug", "info", "warning", "error", "critical"],
+        default="debug",
+        help="Level of logging to the console."
+    )
+    parser.add_argument(
+        "-c", "--codelist",
+        help="Codelist or comma separated list of them for migration."
+    )
+    parser.add_argument(
+        "-a", "--agenda",
+        help="Agenda or comma separated list of them for migration."
+    )
+    parser.add_argument(
+        "-u", "--user",
+        type=int,
+        default=0,
+        help="Joomla! user id for migrated records."
+    )
+    # Process command line arguments
+    global cmdline
+    cmdline = parser.parse_args()
+
+
+def setup_logger():
+    """Configure logging facility."""
+    global logger
+    logging.basicConfig(
+        level=getattr(logging, cmdline.verbose.upper()),
+        format="%(levelname)s:%(name)s: %(message)s",
+    )
+    logger = logging.getLogger(Script.name)
+    logger.info("Script started")
+
+
+def main():
+    """Fundamental control function."""
+    setup_params()
+    setup_cmdline()
+    setup_logger()
+    # Connect to source database
+    if not source_open():
+        return
+    Source.database = db.source_config['database']
+    # Connect to source database
+    if not target_open():
+        return
+    Target.database = db.target_config['database']
+    # ETL process
+    migrate_codelist('stay')
+    logger.info("Script finished")
+
+
+if __name__ == "__main__":
+    main()
--- sql.py
+++ sql.py
@@ -9,38 +9,97 @@ __license__ = 'MIT'
 __maintainer__ = __author__
 __email__ = 'libor.gabaj@gmail.com'
 
-source_codelist_prefix = 'jos_codelist'
-source_agenda_prefix = 'jos_familylist'
-#
-target_codelist_prefix = 'jos_codelist'
-target_agenda_prefix = 'jos_familylist'
+source_table_template_codelist = 'jos_codelist_%(table_root)s'
+source_table_template_agenda = 'jos_familylist_%(table_root)s'
+target_table_template_codelist = 'lgbj_gbjcodes_%(table_root)s'
+target_table_template_agenda = 'lgbj_gbjfamily_%(table_root)s'
 
-source_codelist_read = (
-    "SELECT id AS id"
-    ", created AS created, modified AS modified, published AS state"
-    ", code_desc AS description, code_name AS title, code_abbr AS alias"
-    " FROM %(table_prefix)s_%(table_root)s"
-    )
+# Columns aliases aligned with target column names
+source_fields = {
+    'codelist': (
+        'id, created, modified, published AS state'
+        ', code_name AS title, code_abbr AS alias, code_desc AS description'
+        ),
+    'asset': (
+        'id, created, modified, published AS state'
+        ', item_date AS date_on, item_name AS title, item_desc AS description'
+        ', item_value_euro AS value, item_price_euro AS price'
+        ', item_price_orig AS price_orig, item_currency_id AS id_currency'
+        ', item_domain_id AS id_domain, item_asset_id AS id_asset'
+        ),
+}
 
-source_agenda_read = (
-    "SELECT id AS id"
-    ", created AS created, modified AS modified, published AS state"
-    ", item_desc AS description, item_name AS title, item_abbr AS alias"
-    " FROM %(table_prefix)s_%(table_root)s"
-    )
+# Target columns and their values
+target_fields = {
+    'codelist': {
+        'fields': (
+            'params, metakey, metadesc, metadata'
+            ', id, created, modified, state, description, title, alias'
+            ),
+        'values': (
+            '"", "", "", ""'
+            ', %(id)s, %(created)s, %(modified)s, %(state)s'
+            ', %(description)s, %(title)s, %(alias)s'
+            ),
+    },
+}
+
+target_users = 'created_by = %(user)s, modified_by = %(user)s'
+
+# Map of source codelist table roots to target ones
+map_codelist = {
+    'activity': 'activities',
+    'asset': 'assets',
+    'commodity': 'commodities',
+    'currency': 'currencies',
+    'domain': 'domains',
+    'location': 'locations',
+    'staff': 'staffs',
+    'stay': 'stays',
+    'type': 'types',
+    'unit': 'units',
+}
+
+# Map of source agenda table roots to target ones
+map_agenda = {
+    'asset': 'assets',
+    'event': 'events',
+    'expense': 'expenses',
+    'fuel': 'fuels',
+    'income': 'incomes',
+    'vacation': 'vacations',
+}
 
 
-def query_compose(query_string, table_prefix, table_root):
-    """Compose query string for table name parts.
+def compose_table(table_template, table_root):
+    """Compose source table name for a codelist.
 
     Arguments
     ---------
-    query_string : str
-        Raw query string with parameter placeholders.
-    table_prefix : str
-        Common part of a table name.
+    table_template : str
+        Template for a table name.
     table_root : str
         Specific part of a table name.
+
+    Returns
+    -------
+    str
+        Real table name.
+
+    """
+    table_name = table_template % {'table_root': table_root}
+    return table_name
+
+
+def compose_select(table, fields):
+    """Compose select query string.
+
+    Arguments
+    ---------
+    table : str
+        Real table name.
+    fields : str
+        List of table fields.
 
     Returns
     -------
@@ -49,8 +108,135 @@ def query_compose(query_string, table_prefix, table_root):
         for query parameters.
 
     """
-    query_string = query_string % {
-        'table_prefix': table_prefix,
-        'table_root': table_root,
-        }
-    return query_string
+    query = 'SELECT {} FROM {}'.format(fields, table)
+    return query
+
+
+def compose_insert(table, fields, values):
+    """Compose insert command string.
+
+    Arguments
+    ---------
+    table : str
+        Real table name.
+    fields : str
+        List of table fields.
+    values : dict
+        Dictionary of table fields and their values.
+
+    Returns
+    -------
+    str
+        Query string with real table name. However, it can contain placeholders
+        for query parameters.
+
+    """
+    query = 'INSERT INTO {} ({}) VALUES ({})'.format(table, fields, values)
+    return query
+
+
+def compose_update(table, fields):
+    """Compose update query string.
+
+    Arguments
+    ---------
+    table : str
+        Real table name.
+    fields : str
+        List of table fields.
+
+    Returns
+    -------
+    str
+        Query string with real table name. However, it can contain placeholders
+        for field values.
+
+    """
+    query = 'UPDATE {} SET {}'.format(table, fields)
+    return query
+
+
+def compose_truncate(table):
+    """Compose truncate command string.
+
+    Arguments
+    ---------
+    table : str
+        Real table name.
+
+    Returns
+    -------
+    str
+        Query string with real table name.
+
+    """
+    query = 'TRUNCATE TABLE {}'.format(table)
+    return query
+
+
+def source_codelist(table_root):
+    """Compose source table name for a codelist.
+
+    Arguments
+    ---------
+    table_root : str
+        Specific part of a table name.
+
+    Returns
+    -------
+    str
+        Real table name.
+
+    """
+    return compose_table(source_table_template_codelist, table_root)
+
+
+def source_agenda(table_root):
+    """Compose source table name for an agenda.
+
+    Arguments
+    ---------
+    table_root : str
+        Specific part of a table name.
+
+    Returns
+    -------
+    str
+        Real table name.
+
+    """
+    return compose_table(source_table_template_agenda, table_root)
+
+
+def target_codelist(table_root):
+    """Compose target table name for a codelist.
+
+    Arguments
+    ---------
+    table_root : str
+        Specific part of a table name.
+
+    Returns
+    -------
+    str
+        Real table name.
+
+    """
+    return compose_table(target_table_template_codelist, table_root)
+
+
+def target_agenda(table_root):
+    """Compose target table name for an agenda.
+
+    Arguments
+    ---------
+    table_root : str
+        Specific part of a table name.
+
+    Returns
+    -------
+    str
+        Real table name.
+
+    """
+    return compose_table(target_table_template_agenda, table_root)
